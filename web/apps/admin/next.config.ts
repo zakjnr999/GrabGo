c27b703a5456dd9fc804185da20c3570b0ad235e
Muktar Zakari Junior <zakjnr165@gmail.com>
2026-01-11 00:43:48 +0000

    update

--- web/apps/admin/next.config.ts
+++ web/apps/admin/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone', // Optimized for Render deployment
 };
 
 export default nextConfig;
