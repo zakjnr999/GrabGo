893a3c901430be4ce42473b291940a9f3fa55baf
Muktar Zakari Junior <zakjnr165@gmail.com>
2026-02-04 22:23:20 +0000

    fixed bug relating to prisma

--- backend/services/credit_service.js
+++ backend/services/credit_service.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const prisma = require("../config/prisma");
 const { sendToUser } = require("./fcm_service");
 const { createNotification } = require("./notification_service");
 const { getIO } = require("../utils/socket");
